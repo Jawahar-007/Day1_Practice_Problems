999f94cf830c64f896a65eb75c5eccb818b71d6d
Jawahar-007 <jawaharbhaskaran@gmail.com>
2024-01-20 11:51:58 +0530

    Added: Coin Master

---
+++ src/com/bridgelabz/practiceproblems1/Coinmaster.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.practiceproblems1;
+import java.util.*;
+public class Coinmaster { public static void main(String[] args) {
+    Scanner input= new Scanner(System.in);
+    System.out.println("Enter number of times to flip the coin");
+    int flips= input.nextInt();
+    int headcount=0,tailcount=0;
+    for(int i=1;i<=flips;i++) {
+        double  rand= Math.random();
+        if (rand >= 0 && rand < 0.5) {
+            tailcount++;
+
+        } else {git
+            headcount++;
+        }
+    }
+    System.out.println("Headcount is:"+headcount);
+    System.out.println("Tailcount is:"+tailcount);
+    double headPercentage = ((double) headcount / flips) * 100;
+    System.out.println("Heads percentage=" + headPercentage);
+    double tailPercentage = 100-(headPercentage);
+    System.out.println("Tails percentage=" + tailPercentage);
+
+
+}
+}
